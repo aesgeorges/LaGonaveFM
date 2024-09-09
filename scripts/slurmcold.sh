beec62f2502360cb3df5a979ec87a8b6be895f2b
Alexandre Georges <aes.georges@gmail.com>
2024-09-08 21:15:00 -0700

    updated slurm batch for cold spinup/hot wind

--- scripts/slurmcold.sh
+++ scripts/slurmcold.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Hurricane Matthew Staging Runs
 # Configuration
-#SBATCH --job-name=Matthew2016
+#SBATCH --job-name=
 #SBATCH --account=fc_riser
 #SBATCH --partition=savio2
 #SBATCH --nodes=1
@@ -11,18 +11,82 @@
 #SBATCH --time=12:00:00
 
 ## Command(s) to run
-dt=$(sed -n '22p' fort.15 | awk '{print $1}')
-log="$SLURM_JOB_ID\t\t\t$dt\t\t\t coldstart-full"
-echo -e $log >> runlog.out 
 
-current_dir=$(pwd)
-folder_name=$(basename $current_dir)
+nprocs=24
 
-mkdir ../../logs/$folder_name/$SLURM_JOB_ID
-cp fort.15 ../../logs/$folder_name/$SLURM_JOB_ID/fort.15
+main() {
+    dt=$(sed -n '22p' fort.15 | awk '{print $1}')
+    log="$SLURM_JOB_ID\t\t\t$dt\t\t\t coldstart-full"
+    echo -e $log >> runlog.out 
 
-adcprep --np 24 --partmesh
-adcprep --np 24 --prepall
-mpirun -np 24 padcirc
+    current_dir=$(pwd)
+    folder_name=$(basename $current_dir)
 
-mv slurm-$SLURM_JOB_ID.out ../../logs/$folder_name/$SLURM_JOB_ID/slurm-$SLURM_JOB_ID.out
+    mkdir ../../logs/$folder_name/$SLURM_JOB_ID
+    # cp fort.15 ../../logs/$folder_name/$SLURM_JOB_ID/fort.15
+
+    echo -e "Cold start spinup run with tides ................ "
+    run_cold_spinup
+    echo -e "Hot start run with wind/hurricane forcing ................ "
+    run_hot_wind
+
+    mv slurm-$SLURM_JOB_ID.out ../../logs/$folder_name/$SLURM_JOB_ID/slurm-$SLURM_JOB_ID.out
+}
+
+run_cold_spinup() {
+    log="\t\t\t\t\t\t\t\t\t\t coldstart-spinup"
+    echo -e $log >> runlog.out 
+
+    cp fort.15.coldstart ../../logs/$folder_name/$SLURM_JOB_ID/fort.15.coldstart
+    rm -rf coldstart
+    mkdir coldstart
+    cd coldstart
+    ln -sf ../fort.13
+    ln -sf ../fort.14
+    ln -sf ../fort.15.coldstart ./fort.15
+
+    adcprep --np $nprocs --partmesh
+    adcprep --np $nprocs --prepall
+    mpirun -np $nprocs padcirc
+
+    clean_directory
+    cd ..
+}
+
+run_hot_wind() {
+    log="\t\t\t\t\t\t\t\t\t\t hotstart-wind run"
+    echo -e $log >> runlog.out 
+    
+    cp fort.15.hotstart ../../logs/$folder_name/$SLURM_JOB_ID/fort.15.hotstart
+    rm -rf hotstart
+    mkdir hotstart
+    cd hotstart
+    ln -sf ../fort.14
+    ln -sf ../fort.13
+    ln -sf ../fort.15.hotstart ./fort.15
+    ln -sf ../coldstart/fort.67.nc
+    ln -sf ../fort.22 ./fort.22
+    aswip
+    mv NWS_20_fort.22 fort.22
+
+    adcprep --np $nprocs --partmesh
+    adcprep --np $nprocs --prepall
+    mpirun -np $nprocs padcirc
+
+    clean_directory
+    cd ..
+}
+
+clean_directory() {
+  rm -rf PE*
+  rm -rf partmesh.txt
+  rm -rf metis_graph.txt
+  rm -rf fort.13
+  rm -rf fort.14
+  rm -rf fort.15
+  rm -rf fort.16
+  rm -rf fort.80
+  rm -rf fort.68.nc
+}
+
+main
